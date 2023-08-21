ac23dd248f08465998310e5ae35483bf30dff49a
D4NGK4 <danjosheps.djt98@gmail.com>
2023-08-22 02:42:56 +0800

    added new method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -24,6 +24,22 @@ class Todo(db.Model):
 @app.route('/', methods=['POST', 'GET'])
 
 def index():
+    if request.method == 'POST':
+        task_content = request.form('content')
+        new_task = Todo(content=task_content)
+        
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task!'
+            
+            
+        
+    else:
+        return render_template('index.html')
+    
     return render_template('index.html')
 
 if __name__ == "__main__":
